8aea4e78e01db4110593194e4dbbe4786ed65226
Th0ralfDeFries <th0ralfdefries@gmail.com>
2018-11-09 21:33:38 +0100

    .2

--- app/src/main/java/de/stebadev/notnamedyet/order.java
+++ app/src/main/java/de/stebadev/notnamedyet/order.java
@@ -1,7 +1,16 @@
 package de.stebadev.notnamedyet;
 
-import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.*;
+import java.util.Date;
 
 @Entity(tableName = "orders")
 public class order {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "orderid")
+    private long id;
+
+    @ColumnInfo(name = "datetime")
+    private Date dateTime;
+
+
 }
