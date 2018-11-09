7860ce6fe0d8135b006d1961f26d43a557082d67
Th0ralfDeFries <th0ralfdefries@gmail.com>
2018-11-09 18:48:59 +0100

    .2

---
+++ app/src/main/java/de/stebadev/notnamedyet/customer.java
@@ -0,0 +1,7 @@
+package de.stebadev.notnamedyet;
+
+import android.arch.persistence.room.Entity;
+
+@Entity(tableName = "customers")
+public class customer {
+}
---
+++ app/src/main/java/de/stebadev/notnamedyet/order.java
@@ -0,0 +1,7 @@
+package de.stebadev.notnamedyet;
+
+import android.arch.persistence.room.Entity;
+
+@Entity(tableName = "orders")
+public class order {
+}
